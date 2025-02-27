d0be3aa8661fd7a0222f6091d8a84e8a7e00445d
mrlgcretive <mrlgcreative@gmail.com>
2025-02-27 10:59:16 +0100

    database

---
+++ database/college1.sql
@@ -0,0 +1,615 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : mer. 26 fév. 2025 à 11:52
+-- Version du serveur : 10.4.32-MariaDB
+-- Version de PHP : 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `college1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `attendances`
+--
+
+CREATE TABLE `attendances` (
+  `id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `class_id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `status` enum('present','absent') NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `classes`
+--
+
+CREATE TABLE `classes` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `section` enum('maternelle','primaire','secondaire') NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `classes`
+--
+
+INSERT INTO `classes` (`id`, `nom`, `section`, `created_at`) VALUES
+(1, '1A', 'primaire', '2025-02-16 16:27:32'),
+(2, '2B', 'primaire', '2025-02-16 16:27:32');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `comptable`
+--
+
+CREATE TABLE `comptable` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `prenom` varchar(255) NOT NULL,
+  `contact` varchar(20) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `adresse` text NOT NULL,
+  `section` enum('primaire','secondaire','maternel') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `cours`
+--
+
+CREATE TABLE `cours` (
+  `id` int(11) NOT NULL,
+  `titre` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `professeur_id` int(11) NOT NULL,
+  `classe_id` int(11) NOT NULL,
+  `section` enum('maternelle','primaire','secondaire') NOT NULL,
+  `option_` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `cours`
+--
+
+INSERT INTO `cours` (`id`, `titre`, `description`, `professeur_id`, `classe_id`, `section`, `option_`, `created_at`) VALUES
+(2, 'Maths', 'Algbre', 1, 2, 'secondaire', '', '2025-02-26 07:19:27'),
+(3, 'math', 'physique', 1, 2, 'secondaire', 'Mecanique auto', '2025-02-26 09:08:19');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `directeur`
+--
+
+CREATE TABLE `directeur` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `prenom` varchar(255) NOT NULL,
+  `contact` varchar(20) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `adresse` text NOT NULL,
+  `section` enum('primaire','secondaire','maternel') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `directrice`
+--
+
+CREATE TABLE `directrice` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `prenom` varchar(255) NOT NULL,
+  `contact` varchar(20) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `adresse` text NOT NULL,
+  `section` enum('primaire','secondaire','maternel') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `eleves`
+--
+
+CREATE TABLE `eleves` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `prenom` varchar(50) NOT NULL,
+  `date_naissance` date NOT NULL,
+  `sexe` enum('M','F') NOT NULL,
+  `section` enum('maternelle','primaire','secondaire') NOT NULL,
+  `option` varchar(255) DEFAULT NULL,
+  `adresse` varchar(255) NOT NULL,
+  `contact` varchar(20) NOT NULL,
+  `id_parent` int(11) DEFAULT NULL,
+  `classe` enum('maternelle','primaire','secondaire') NOT NULL,
+  `frais_status` enum('en_ordre','non_en_ordre') NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `employes`
+--
+
+CREATE TABLE `employes` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `prenom` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `contact` varchar(20) NOT NULL,
+  `poste` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `fee_payments`
+--
+
+CREATE TABLE `fee_payments` (
+  `id` int(11) NOT NULL,
+  `eleve_id` int(11) NOT NULL,
+  `frais_id` int(11) NOT NULL,
+  `amount_paid` decimal(10,2) NOT NULL,
+  `payment_date` date NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `frais`
+--
+
+CREATE TABLE `frais` (
+  `id` int(11) NOT NULL,
+  `montant` decimal(10,2) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `section` enum('primaire','secondaire','maternelle') DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `frais`
+--
+
+INSERT INTO `frais` (`id`, `montant`, `description`, `section`, `created_at`) VALUES
+(1, 1000.00, 'Loge', NULL, '2025-02-23 16:23:08'),
+(2, 500.00, 'fip', 'primaire', '2025-02-23 16:27:42'),
+(3, 10.00, 'Achat', NULL, '2025-02-23 16:29:31'),
+(4, 42.00, 'Mensuels', 'secondaire', '2025-02-23 16:34:21'),
+(5, 200.00, 'FIP', 'primaire', '2025-02-23 18:23:08'),
+(6, 300.00, 'Mensuels', 'maternelle', '2025-02-23 18:30:43'),
+(7, 700.00, 'Loge', 'primaire', '2025-02-25 10:35:59');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `historique`
+--
+
+CREATE TABLE `historique` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `action` varchar(255) NOT NULL,
+  `date_action` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `options`
+--
+
+CREATE TABLE `options` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `description` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `paiements`
+--
+
+CREATE TABLE `paiements` (
+  `id` int(11) NOT NULL,
+  `professeur_id` int(11) NOT NULL,
+  `montant` decimal(10,2) NOT NULL,
+  `date_paiement` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `parents`
+--
+
+CREATE TABLE `parents` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `prenom` varchar(50) NOT NULL,
+  `contact` varchar(20) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `parents`
+--
+
+INSERT INTO `parents` (`id`, `nom`, `prenom`, `contact`, `email`, `created_at`) VALUES
+(1, 'ochiwa', 'sky', '0979099031', 'ochiwa@gmail.com', '2025-02-23 19:03:00');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `prefet`
+--
+
+CREATE TABLE `prefet` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `prenom` varchar(255) NOT NULL,
+  `contact` varchar(20) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `adresse` text NOT NULL,
+  `section` enum('primaire','secondaire','maternel') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `professeurs`
+--
+
+CREATE TABLE `professeurs` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `prenom` varchar(255) NOT NULL,
+  `contact` varchar(20) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `adresse` text NOT NULL,
+  `classe_id` int(11) DEFAULT NULL,
+  `cours_id` int(11) DEFAULT NULL,
+  `section` enum('primaire','secondaire','maternel') DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `professeurs`
+--
+
+INSERT INTO `professeurs` (`id`, `nom`, `prenom`, `contact`, `email`, `adresse`, `classe_id`, `cours_id`, `section`) VALUES
+(1, 'ochiwa', 'sky', '0979099031', 'ochiwa@gmail.com', 'lumumba', 1, NULL, 'primaire');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` enum('admin','director','directrice','prefet','comptable','parent') NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `role`, `created_at`) VALUES
+(1, 'glo', '$2y$10$OmXJdG5OuOGJ6jU7YJGoZ.PPDH7KGMse4LaJShafiiFiJOTd6mLlO', 'admin', '2025-02-16 09:45:35');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `attendances`
+--
+ALTER TABLE `attendances`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `student_id` (`student_id`),
+  ADD KEY `class_id` (`class_id`);
+
+--
+-- Index pour la table `classes`
+--
+ALTER TABLE `classes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `comptable`
+--
+ALTER TABLE `comptable`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `cours`
+--
+ALTER TABLE `cours`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `professeur_id` (`professeur_id`),
+  ADD KEY `classe_id` (`classe_id`);
+
+--
+-- Index pour la table `directeur`
+--
+ALTER TABLE `directeur`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `directrice`
+--
+ALTER TABLE `directrice`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `eleves`
+--
+ALTER TABLE `eleves`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_parent` (`id_parent`);
+
+--
+-- Index pour la table `employes`
+--
+ALTER TABLE `employes`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Index pour la table `fee_payments`
+--
+ALTER TABLE `fee_payments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `eleve_id` (`eleve_id`),
+  ADD KEY `frais_id` (`frais_id`);
+
+--
+-- Index pour la table `frais`
+--
+ALTER TABLE `frais`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `historique`
+--
+ALTER TABLE `historique`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Index pour la table `options`
+--
+ALTER TABLE `options`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `paiements`
+--
+ALTER TABLE `paiements`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `professeur_id` (`professeur_id`);
+
+--
+-- Index pour la table `parents`
+--
+ALTER TABLE `parents`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Index pour la table `prefet`
+--
+ALTER TABLE `prefet`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `professeurs`
+--
+ALTER TABLE `professeurs`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_classe_id` (`classe_id`),
+  ADD KEY `fk_cours_id` (`cours_id`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `attendances`
+--
+ALTER TABLE `attendances`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `classes`
+--
+ALTER TABLE `classes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `comptable`
+--
+ALTER TABLE `comptable`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `cours`
+--
+ALTER TABLE `cours`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT pour la table `directeur`
+--
+ALTER TABLE `directeur`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `directrice`
+--
+ALTER TABLE `directrice`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `eleves`
+--
+ALTER TABLE `eleves`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `employes`
+--
+ALTER TABLE `employes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `fee_payments`
+--
+ALTER TABLE `fee_payments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `frais`
+--
+ALTER TABLE `frais`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT pour la table `historique`
+--
+ALTER TABLE `historique`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `options`
+--
+ALTER TABLE `options`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `paiements`
+--
+ALTER TABLE `paiements`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `parents`
+--
+ALTER TABLE `parents`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT pour la table `prefet`
+--
+ALTER TABLE `prefet`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `professeurs`
+--
+ALTER TABLE `professeurs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `attendances`
+--
+ALTER TABLE `attendances`
+  ADD CONSTRAINT `attendances_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `attendances_ibfk_2` FOREIGN KEY (`class_id`) REFERENCES `classes` (`id`);
+
+--
+-- Contraintes pour la table `cours`
+--
+ALTER TABLE `cours`
+  ADD CONSTRAINT `cours_ibfk_1` FOREIGN KEY (`professeur_id`) REFERENCES `professeurs` (`id`),
+  ADD CONSTRAINT `cours_ibfk_2` FOREIGN KEY (`classe_id`) REFERENCES `classes` (`id`);
+
+--
+-- Contraintes pour la table `eleves`
+--
+ALTER TABLE `eleves`
+  ADD CONSTRAINT `eleves_ibfk_1` FOREIGN KEY (`id_parent`) REFERENCES `parents` (`id`);
+
+--
+-- Contraintes pour la table `fee_payments`
+--
+ALTER TABLE `fee_payments`
+  ADD CONSTRAINT `fee_payments_ibfk_1` FOREIGN KEY (`eleve_id`) REFERENCES `eleves` (`id`),
+  ADD CONSTRAINT `fee_payments_ibfk_2` FOREIGN KEY (`frais_id`) REFERENCES `frais` (`id`);
+
+--
+-- Contraintes pour la table `historique`
+--
+ALTER TABLE `historique`
+  ADD CONSTRAINT `historique_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Contraintes pour la table `paiements`
+--
+ALTER TABLE `paiements`
+  ADD CONSTRAINT `paiements_ibfk_1` FOREIGN KEY (`professeur_id`) REFERENCES `professeurs` (`id`);
+
+--
+-- Contraintes pour la table `professeurs`
+--
+ALTER TABLE `professeurs`
+  ADD CONSTRAINT `fk_classe_id` FOREIGN KEY (`classe_id`) REFERENCES `classes` (`id`),
+  ADD CONSTRAINT `fk_cours_id` FOREIGN KEY (`cours_id`) REFERENCES `cours` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
